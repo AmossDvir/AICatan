d1c9f39a529631852b96fee8b7e3facf14ff7e5d
Boaz Bibi <boazbibi@gmail.com>
2020-07-27 01:29:09 +0300

    board.py almost finished

--- logic/board.py
+++ logic/board.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from collections import defaultdict
 import hexgrid
 
 # Some definitions:
@@ -58,10 +59,15 @@ class CatanBoard:
         # Location of the robber:
         self.robber_tile = hexgrid.tile_id_to_coord(tiles.index(HexType.DESERT) + 1)
 
-        # Information we keep to simplify calculations:
+        # Information we keep to avoid complex calculations:
         self.legal_nodes = hexgrid.legal_node_coords()
+        self.player_harbors = defaultdict(list) # player -> list of HarborType
 
     def get_empty_edges_around_node(self, node):
+        """
+        :param node: The index number of the node
+        :return: Array of edges (can be empty)
+        """
         ret = []
         edges = get_edges_adjacent_to_node(node)
         for edge in edges:
@@ -71,9 +77,15 @@ class CatanBoard:
 
     # Methods for the setup phase of the game:
     def setup_get_available_settlement_nodes(self):
+        """
+        returns all the legal places to put a settlement in
+        """
         return self.legal_nodes.copy()
 
     def setup_place_settlement_and_road(self, player_id, node_id, edge_id):
+        """
+        Places a settlement and road on the board (used in the setup phase of the game)
+        """
         if not node_id in self.legal_nodes:
             raise ValueError("Couldn't place settlement, illegal node id.")
         if edge_id in self.roads.keys():
@@ -84,6 +96,74 @@ class CatanBoard:
             self.legal_nodes.remove(node)
         self.roads[edge_id] = player_id
         self.pieces[node_id] = (player_id, PieceType.SETTLEMENT)
+        # TODO: Return the resources from the city
+        
+
+    def build_settlement(self, player_id, node_id):
+        """
+        :param player_id: The id of the player that builds the settlement
+        :node_id: The index of the node we build in
+        """
+        # Make sure the node is legal
+        if not node_id in self.legal_nodes:
+            raise ValueError("Couldn't place settlement, illegal node id.")
+        # Make sure there is a road that belongs to the player around the node
+        if player_id not in [self.roads.get(e, -1) for e in get_edges_adjacent_to_node(node_id)]
+
+        self.legal_nodes.remove(node_id)
+        # Remove adjecent nodes (since it's illegal to build next to a settlement)
+        for node in get_nodes_adjacent_node(node_id):
+            self.legal_nodes.remove(node)
+        self.pieces[node_id] = (player_id, PieceType.SETTLEMENT)
+
+    def build_road(self, player_id, edge_id):
+        """
+        :param player_id: The id of the player that builds the road
+        :node_id: The index of the edge we buildthe road in
+        """
+        # Make sure the position is clear:
+        if self.roads.get(edge_id, -1) != -1:
+            raise ValueError("Couldn't place road, another road already exist in the edge.")
+        # Make sure the player have another road touching this road:
+        if player_id not in [self.roads.get(e, -1) for e in get_edges_adjacent_to_edge(edge_id)]
+            raise ValueError("Couldn't place road, the player own no adjecent roads.")
+
+    def distribute_resources(self, rolled_number):
+        """
+        :param rolled_number: A number from 2 to 12, the result of the dice 
+        :return: A dict player_id -> len 5 list [WOOD, BRICK, SHEEP, WHEAT, ORE]
+        """
+        pass
+
+    # Methods for the game_state:
+    def get_player_ports(self, player_id):
+        """
+        :param player_id: The player we want the info about
+        :return: A list of HarborType that describes what kind of harbors the player control
+        """
+        pass
+
+    def get_player_legal_settlement_nodes(self, player_id):
+        """
+        :param player_id: The player we want the info about
+        :return: A list of nodes that the player allowed to build settlement in
+        """
+        pass
+
+    def get_player_legal_road_edges(self, player_id):
+        """
+        :param player_id: The player we want the info about
+        :return: A list of edges that the player in build road in
+        """
+        pass
+
+    def get_player_legal_city_nodes(self, player_id):
+        """
+        :param player_id: The player we want the info about
+        :return: A list of nodes that the player allowed to build settlement in
+        """
+        pass
+
 
 
 # Auxiliary functions for hexgrid
@@ -99,8 +179,12 @@ def get_edges_adjacent_to_node(node):
     else:
         return [node, node - 0x01, node - 0x11]
 
-if __name__ == "__main__":
-    board = CatanBoard()
+def get_edges_adjacent_to_edge(edge):
+    edges = []
+    nodes = hexgrid.nodes_touching_edge(edge)
+    for node in nodes:
+        edges += get_edges_adjacent_to_node(node)
+    return edges
 
 
 
